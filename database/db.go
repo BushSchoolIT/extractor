8fba0fbfd9dbf27fcf75f02d9b4a337325c460ec
Michael <Michael.Lindner@bush.edu>
2025-07-18 12:55:44 -0700

    removes calls to removeNull for primaryKeys as in most cases that means something has gone wrong. Also renames InsertEmails to InsertParentEmails as it contains a postgres query specific to the parents table

--- database/db.go
+++ database/db.go
@@ -50,7 +50,7 @@ func (db *State) Close() error {
 	return db.Conn.Close(*db.Ctx)
 }
 
-func (db *State) InsertEmails(t blackbaud.UnorderedTable) error {
+func (db *State) InsertParentEmails(t blackbaud.UnorderedTable) error {
 	tx, err := db.Conn.BeginTx(*db.Ctx, pgx.TxOptions{})
 	if err != nil {
 		return err
@@ -65,7 +65,7 @@ func (db *State) InsertEmails(t blackbaud.UnorderedTable) error {
 		tx.Rollback(*db.Ctx)
 		return fmt.Errorf("failed to truncate parent db: %v, cmd: %s", err, cmd.String())
 	}
-	// remove null primary keys
+	// remove null primary keys as not all parents have valid emails in Blackbaud
 	removeNull(primaryKeys, t)
 	query := fmt.Sprintf(`
 	INSERT INTO parents (%s) VALUES (%s)
@@ -93,8 +93,6 @@ func (db *State) InsertAttendance(t blackbaud.UnorderedTable) error {
 		"id": true,
 	}
 
-	// remove null primary keys
-	removeNull(primaryKeys, t)
 	query := fmt.Sprintf(`
 	INSERT INTO attendance (%s) VALUES (%s)
 	ON CONFLICT (%s)
@@ -165,8 +163,6 @@ func (db *State) TranscriptOps(t blackbaud.UnorderedTable, startYear int, endYea
 		"course_id":       true,
 		"grade_id":        true,
 	}
-	// remove null primary keys
-	removeNull(primaryKeys, t)
 	query := fmt.Sprintf(`
 	INSERT INTO transcripts (%s) VALUES (%s)
 	ON CONFLICT (%s)
@@ -210,11 +206,9 @@ func (db *State) EnrollmentOps(enrolled blackbaud.UnorderedTable, departed black
 	if err != nil {
 		return err
 	}
-	// remove null primary keys
 	primaryKeys := map[string]bool{
 		"student_user_id": true,
 	}
-	removeNull(primaryKeys, enrolled)
 	enrolledInsert := fmt.Sprintf(`
 	INSERT INTO enrollment (%s) VALUES (%s)
 	ON CONFLICT (%s)
@@ -298,8 +292,6 @@ func (db *State) TranscriptCommentOps(t blackbaud.UnorderedTable) error {
 	primaryKeys := map[string]bool{
 		"student_user_id": true,
 	}
-	// remove null primary keys
-	removeNull(primaryKeys, t)
 	query := fmt.Sprintf(`
 	INSERT INTO transcript_comments (%s) VALUES (%s)
 	ON CONFLICT (%s)
